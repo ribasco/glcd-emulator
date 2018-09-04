87be9b52828f3f5942aa43e0a5829f3b38457611
Rafael Ibasco <ribasco@gmail.com>
2018-09-04 21:55:52 +0800

    Added class comment header

--- src/main/java/com/ibasco/glcdemu/emulators/st7920/ST7920Instruction.java
+++ src/main/java/com/ibasco/glcdemu/emulators/st7920/ST7920Instruction.java
@@ -2,7 +2,12 @@ package com.ibasco.glcdemu.emulators.st7920;
 
 import com.ibasco.glcdemu.emulators.GlcdInstruction;
 
-public class ST7920Instruction extends GlcdInstruction<ST7920InstructionFlags> {
+/**
+ * Base class for ST7920 instruction
+ *
+ * @author Rafael Ibasco
+ */
+abstract public class ST7920Instruction extends GlcdInstruction<ST7920InstructionFlags> {
     public ST7920Instruction(ST7920InstructionFlags flag, byte value) {
         super(flag, value);
     }
