27cc6781f17a3ae56949d2a40b10a2bf914a6344
Rafael Ibasco <ribasco@gmail.com>
2018-09-14 22:40:28 +0800

    Added time unit property for flexibility

--- src/main/java/com/ibasco/glcdemu/utils/FpsCounter.java
+++ src/main/java/com/ibasco/glcdemu/utils/FpsCounter.java
@@ -1,5 +1,6 @@
 package com.ibasco.glcdemu.utils;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
@@ -11,7 +12,42 @@ public class FpsCounter {
 
     private Consumer<Integer> countListener;
 
-    public void setCountListener(Consumer<Integer> listener) {
+    private long interval;
+
+    private TimeUnit timeUnit;
+
+    private int lastCount;
+
+    public FpsCounter() {
+        this(1, TimeUnit.SECONDS);
+    }
+
+    public FpsCounter(long interval, TimeUnit timeUnit) {
+        this.interval = timeUnit.toNanos(interval);
+        this.timeUnit = timeUnit;
+    }
+
+    public int getLastCount() {
+        return lastCount;
+    }
+
+    public long getInterval() {
+        return interval;
+    }
+
+    public void setInterval(long interval) {
+        this.interval = interval;
+    }
+
+    public TimeUnit getTimeUnit() {
+        return timeUnit;
+    }
+
+    public void setTimeUnit(TimeUnit timeUnit) {
+        this.timeUnit = timeUnit;
+    }
+
+    public void setListener(Consumer<Integer> listener) {
         this.countListener = listener;
     }
 
@@ -20,11 +56,22 @@ public class FpsCounter {
     }
 
     public void pulse() {
-        long current = System.currentTimeMillis();
-        if ((current - previous) >= 1000) {
-            if (countListener != null)
-                countListener.accept(counter.getAndSet(0));
-            previous = current;
+        pulse(TimeUnit.SECONDS.toNanos(1));
+    }
+
+    /**
+     * Sends a pulse to regularly check on the current state
+     *
+     * @param now
+     *         The pulse time in nanoseconds
+     */
+    public synchronized void pulse(long now) {
+        if ((now - previous) >= interval) {
+            lastCount = counter.getAndSet(0);
+            if (countListener != null) {
+                countListener.accept(lastCount);
+            }
+            previous = now;
         }
     }
 }
